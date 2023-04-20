a681d7b1e5dd1713b4bb911fea090686ea0cf122
Darley Peralta <ordeal00s@gmail.com>
2023-04-20 19:31:10 -0400

    darleyP

--- src/components/card.js
+++ src/components/card.js
@@ -6,7 +6,7 @@ import axios from "axios";
 // The tags used, the hierarchy of elements and their attributes must match the provided markup exactly!
 // The text inside elements will be set using their `textContent` property (NOT `innerText`).
 // Add a listener for click events so that when a user clicks on a card, the headline of the article is logged to the console.
-//
+
 // <div class="card">
 //   <div class="headline">{ headline }</div>
 //   <div class="author">
@@ -16,7 +16,7 @@ import axios from "axios";
 //     <span>By { authorName }</span>
 //   </div>
 // </div>
-//
+
 const Card = (article) => {
   let headline = article.headline
   let authorName = article.authorName
@@ -45,25 +45,42 @@ const Card = (article) => {
 
     span.textContent = authorName ;
     headlineDiv.textContent =  headline ;
-
+console.log(cardDiv)
     return cardDiv
   
 
 
- // TASK 6
-  // ---------------------
-  // Implement this function that takes a css selector as its only argument.
-  // It should obtain articles from this endpoint: `http://localhost:5001/api/articles` (test it with console.log!!).
-  // However, the articles do not come organized in a single, neat array. Inspect the response closely!
-  // Create a card from each and every article object in the response, using the Card component.
-  // Append each card to the element in the DOM that matches the selector passed to the function.
-  //
+//  TASK 6
+//   ---------------------
+//   Implement this function that takes a css selector as its only argument.
+//   It should obtain articles from this endpoint: `http://localhost:5001/api/articles` (test it with console.log!!).
+//   However, the articles do not come organized in a single, neat array. Inspect the response closely!
+//   Create a card from each and every article object in the response, using the Card component.
+//   Append each card to the element in the DOM that matches the selector passed to the function.
+  
 }
 
 const cardAppender = (selector) => {
+  let selc = document.querySelector(selector);
  axios.get('http://localhost:5001/api/articles')
  .then( res => {
-  
+  let articles = res.data.articles;
+    articles.javascript.forEach( obj => {
+    selc.appendChild(Card(obj))
+    })
+    articles.bootstrap.forEach( obj => {
+      selc.appendChild(Card(obj))
+      })
+      articles.technology.forEach( obj => {
+        selc.appendChild(Card(obj))
+        })
+        articles.jquery.forEach( obj => {
+          selc.appendChild(Card(obj))
+          })
+          articles.node.forEach( obj => {
+            selc.appendChild(Card(obj))
+            })
+    console.log(articles)
  })
 }
 
--- src/components/tabs.js
+++ src/components/tabs.js
@@ -15,33 +15,35 @@ import axios from "axios";
 //
 const Tabs = (topics) => {
   let topicDiv = document.createElement('div');
-  
-  topicDiv.classList.add('topics');
 
-  topics.forEach(topic => {
+  topicDiv.classList.add('topics');
+  topics.forEach(obj => {
     let tabDiv = document.createElement('div');
     tabDiv.classList.add('tab');
     topicDiv.appendChild(tabDiv);
-    tabDiv.textContent = topic;
+    tabDiv.textContent = obj;
   });
+  
   return topicDiv
 }
 
-  // TASK 4
-  // ---------------------
-  // Implement this function which takes a css selector as its only argument.
-  // It should obtain topics from this endpoint: `http://localhost:5001/api/topics` (test it with a console.log!).
-  // Find the array of topics inside the response, and create the tabs using the Tabs component.
-  // Append the tabs to the element in the DOM that matches the selector passed to the function.
-  //
-const tabsAppender = (selector) => {
-axios.get('http://localhost:5001/api/topics')
-.then(res => {
-  const sect = document.querySelector(selector);
-  const tabss = Tabs(res.data);
-  sect.appendChild(tabss)
+// TASK 4
+// ---------------------
+// Implement this function which takes a css selector as its only argument.
+// It should obtain topics from this endpoint: `http://localhost:5001/api/topics` (test it with a console.log!).
+// Find the array of topics inside the response, and create the tabs using the Tabs component.
+// Append the tabs to the element in the DOM that matches the selector passed to the function.
 
-})
+const tabsAppender = (selector) => {
+  let selc = document.querySelector(selector)
+  axios.get('http://localhost:5001/api/topics')
+    .then(res => {
+      let tabv = Tabs(res.data.topics)
+      selc.appendChild(tabv)
+    })
+    .catch(error => {
+      console.error(error)
+    })
 }
-console.log(tabsAppender({"topics":["javascript","bootstrap","technology","jquery","node.js"]}))
+
 export { Tabs, tabsAppender }
